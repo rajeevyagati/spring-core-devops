5e6b65af196717eab37d7163ce0c2cfff59c3aca
Rajeevalochan Yagati <rajeevalochan.yagati@gmail.com>
2020-05-03 13:46:59 +0530

    testing CI build

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,7 +9,7 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change test
+		//this is a whitespace change test 123
 		// another test...
 	}
 }
